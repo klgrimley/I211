381de65d20ac6f4b404706f1dea8858ce94488d5
Kevin <klgrimley@hotmail.com>
2014-04-16 08:22:38 -0400

    change base_url based of file structure

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
 require_once ("application/autoloader.class.php");
 
 //define a constant for the application url; you may need to modify the value for your system.
-define("base_url", "http://localhost/I211/kungfupanda_mvc");
+define("base_url", "http://localhost/GitHub/final_project_i211");
 
 Dispatcher::dispatch();
