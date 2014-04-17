e43d8d597116fead29f84110ffc0fa5342167242
Kevin <klgrimley@hotmail.com>
2014-04-16 21:43:24 -0400

    Kolton

--- views/welcome/index.class.php
+++ views/welcome/index.class.php
@@ -11,18 +11,49 @@ class Welcome_Index extends IndexView {
 
     public function display() {
         parent::displayHeader("Music");
-        
-
-        ?>    
+         ?>    
         
            
-                        <ul>
-                            <li><a href="<?= base_url ?>/music/index">Listen to music</a></li>
-                            <li>Stream music</li>
-                        </ul>
-                   
 
-        <br>
+
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Listen Up</title>
+<link href="includes/style.css" rel="stylesheet" type="text/css">
+<link href='http://fonts.googleapis.com/css?family=Dosis:200,400' rel='stylesheet' type='text/css'>
+</head>
+
+<body bgcolor="#ff3333">
+<div id="header">
+<div id="logo">
+<img src="includes/images/title.gif" width="204" height="68">
+</div>
+</div>
+
+<div id="wrapper">
+
+    <img src="includes/images/stream.gif" width="280" height="352"> 
+ <a href="<?= base_url ?>/music/index/"><img src="includes/images/purchase.gif" width="258" height="344"></a> </div>
+
+
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         <?php
                 parent::displayFooter();
        
