f66c00d78b89d2bcebdb8e50c6f77f34b934ea92
Kevin <klgrimley@hotmail.com>
2014-04-23 11:33:50 -0400

    formatted detail page

--- views/album/detail/detail.class.php
+++ views/album/detail/detail.class.php
@@ -14,37 +14,41 @@ class Album_Detail extends IndexView {
         parent::displayHeader("Album Details");
 
         //retrieve album details by calling get methods
-        
         ?>
-
-        <div id="main_header">
+        <div class="detail_wraper">
             <?php
-                 $album_title = $musics[0]['album_title'];
-                 echo "<h3>$album_title</h3>";
-            ?>
-        </div>
-
-        <!-- display album details -->
-        Songs:
-        <div class='song_list'>
-        <ul>
-        <?php
-        foreach ($musics as $count => $music) {
-                //$id = $id->getId();
-                $song_title = $music['song_name'];
-                $audio = $music['audio'];
-                
-                echo "<li><a href='". ABSOLUTE_PATH ."'/includes/audio/$audio>$song_title</a></li>";
-            }
+            $artist = $musics[0]['artist'];
+            $description = $musics[0]['description'];
+            $album_title = $musics[0]['album_title'];
             $image = $musics[0]['image'];
-            
-            
-            echo "<img src='" . base_url . "/includes/album_art/$image' /></a></p>";
-           
-        ?>
-        </ul>
+            echo "<h3>$artist</h3>";
+            ?>
+            <div class='song_list' style='float: right;'>
+                <h4><?= $album_title ?></h4>
+                <h5>Track Listing:</h5>
+                <ol>
+                    <?php
+                    foreach ($musics as $count => $music) {
+                        //$id = $id->getId();
+                        $song_title = $music['song_name'];
+                        $audio = $music['audio'];
+
+                        echo "<li><a href='" . ABSOLUTE_PATH . "'/includes/audio/$audio>$song_title</a></li>";
+                    }
+                    ?>
+                </ol>
+            </div>
+            <img class="album_cover" src="<?= base_url ?>/includes/album_art/<?= $image ?>" style="float: right;" />
+            <div class='detail_information'>
+                <h4>About the Band:</h4>
+                <p><?= $description ?></p>
+            </div>
+
+            <!-- display album details -->
+
+
+            <a href="<?= base_url ?>/index">Back to album list</a>
         </div>
-        <a href="<?= base_url ?>/index">Back to album list</a>
         <?php
         //display page footer
         parent::displayFooter();
